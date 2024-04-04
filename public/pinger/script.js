f07e948964f9cdd450ab3857281f3a97d7005313
Anatoliy Oblaukhov <mail@anmiles.net>
2024-04-04 19:38:01 +0300

    pinger * refactor * add urls

---
+++ public/pinger/script.js
@@ -0,0 +1,71 @@
+const timeout = 5000;
+
+async function ping(url) {
+	return new Promise((resolve, reject) => {
+		const img = new Image();
+		const timer = setTimeout(() => reject(timeout), timeout);
+		const startTime = new Date().getTime();
+		img.onload = img.onerror = () => { clearTimeout(timer); resolve((new Date().getTime() - startTime)) };
+		img.src = url + '?t=' + Math.random().toString().slice(2);
+	})
+}
+
+async function start() {
+	const textarea = document.querySelector('textarea');
+
+	const allUrls = {
+		sbg: {
+			game_app : 'https://sbg-game.ru/app',
+			game_desktop : 'https://sbg-game.ru/app/intel.js',
+			game_mobile : 'https://sbg-game.ru/app/script.js',
+			lib_ol: 'https://cdn.jsdelivr.net/npm/ol@v8.1.0/dist/ol.js',
+			lib_splide: 'https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/js/splide.min.js',
+			lib_toastify: 'https://cdn.jsdelivr.net/npm/toastify-js',
+			lib_telegram: 'https://telegram.org/js/telegram-widget.js?21',
+			fonts_manrope: 'https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap',
+			fonts_material: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined',
+			scripts_cui : 'https://raw.githubusercontent.com/nicko-v/sbg-cui/main/index.js',
+			scripts_eui : 'https://github.com/egorantonov/sbg-enhanced/releases/latest/download/eui.user.js',
+			plus_desktop : 'https://anmiles.net/userscripts/sbg.plus.user.js',
+			plus_mobile : 'https://anmiles.net/userscripts/sbg.plus.user.min.js',
+			plus_dist_desktop : 'https://raw.githubusercontent.com/anmiles/userscripts/main/dist/sbg.plus.user.js',
+			plus_dist_mobile : 'https://raw.githubusercontent.com/anmiles/userscripts/main/dist/sbg.plus.user.min.js',
+			userscripts: 'https://anmiles.net/userscripts',
+			pages_home: 'https://anmiles.net',
+			pages_origin: 'https://anmiles.github.io',
+			other_home: 'https://react.dev',
+			other_origin: 'https://facebook.github.io/react',
+		}
+	};
+
+	const urls = allUrls[location.search.slice(1)];
+	let total = 0;
+
+	if (urls) {
+		const maxKeyLength = Math.max(...Object.keys(urls).map(key => key.length));
+		textarea.value = '';
+
+		for (const key in urls) {
+			textarea.value += key + ' '.repeat(maxKeyLength - key.length) + ' => ';
+
+			try {
+				const result = await ping(urls[key]);
+				total += result;
+				textarea.value += `PASS: ${result}ms\n`;
+			} catch (result) {
+				total += result;
+				textarea.value += `FAIL: ${result}ms timeout\n`;
+			}
+		}
+
+		textarea.value += '-'.repeat(maxKeyLength) + '\n';
+		textarea.value += 'TOTAL: ' + total + 'ms';
+	}
+};
+
+document.querySelector('button').addEventListener('click', (ev) => {
+	ev.preventDefault();
+	ev.target.disabled = true;
+	start().then(() => ev.target.disabled = false);
+	return false;
+});
