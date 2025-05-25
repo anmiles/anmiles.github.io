4fabf3642ec1618c1efb2c7e7036b0dbab40fa7b
Anatoliy Oblaukhov <mail@anmiles.net>
2025-05-25 12:27:56 +0300

    Automate updating versions

--- scripts/updateHTML.js
+++ scripts/updateHTML.js
@@ -1,16 +1,23 @@
 const fs = require('fs');
+const path = require('path');
 const moment = require('moment');
 require('@anmiles/prototypes');
 
 const root = './public';
 const exts = [ '.htm', '.html' ];
 const time = moment().format('yyyy-MM-DD-HH-mm-ss');
+const versions = new Map();
+
+fs.readdirSync(path.join(root, 'libs')).forEach(filename => {
+	const match = filename.match(/^([A-Za-z0-9\-]+?)-([\d\.]+?)\.\D/);
+	if (match) {
+		versions.set(match[1], match[2]);
+	}
+})
 
 fs.recurse(root, { file: (filepath) => {
 	if (exts.filter(ext => filepath.endsWith(ext)).length > 0) {
 		updateHTML(filepath);
-		updateVersion(filepath, time);
-		insertAnalytics(filepath);
 	}
 }});
 
@@ -18,7 +25,7 @@ function updateHTML(filepath) {
 	const originalText = fs.readFileSync(filepath).toString();
 
 	let text = originalText;
-	text = updateVersion(text);
+	text = updateVersions(text);
 	text = insertAnalytics(text);
 
 	if (text !== originalText) {
@@ -27,21 +34,28 @@ function updateHTML(filepath) {
 	}
 }
 
-function updateVersion(text) {
-	return text.replace(/\{VERSION\}/g, time);
+function updateVersions(text) {
+	text = text.replaceAll('{VERSION}', time);
+
+	versions.forEach((version, lib) => {
+		text = text.replaceAll(new RegExp(`(?<=\\/libs\\/${lib.regexEscape()}-)([\\d\\.]+?)(?=\\.\\D)`, 'g'), version);
+	});
+	return text;
 }
 
 function insertAnalytics(text) {
-	return text.replace('</body>', `\
-<!-- Google tag (gtag.js) -->
-<script type="text/javascript" async src="https://www.googletagmanager.com/gtag/js?id=G-C66BR79P9J"></script>
-<script type="text/javascript">
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-C66BR79P9J');
-</script>
+	const analytics = [
+		'<!-- Google tag (gtag.js) -->',
+		'<script type="text/javascript" async src="https://www.googletagmanager.com/gtag/js?id=G-C66BR79P9J"></script>',
+		'<script type="text/javascript">',
+		'window.dataLayer = window.dataLayer || [];',
+		'function gtag(){dataLayer.push(arguments);}',
+		'gtag(\'js\', new Date());',
+		'gtag(\'config\', \'G-C66BR79P9J\');',
+		'</script>',
+	];
 
-</body>\
-`);
+	return text.includes(analytics[0])
+		? text
+		: text.replace(/(\r?\n)*<\/body>/, '\n\n' + analytics.join('\n') + '\n\n</body>');
 }
